9f9095fe13604814b1b5982f2f64d3712d417a64
luigari <agalvisr@gmail.com>
2024-05-17 14:06:49 -0500

    Cambio Dev C

--- src/PartsUnlimited.Mobile/PartsUnlimited/Properties/AssemblyInfo.cs
+++ src/PartsUnlimited.Mobile/PartsUnlimited/Properties/AssemblyInfo.cs
@@ -1,6 +1,6 @@
 ﻿using System.Resources;
 using System.Reflection;
-
+//Cambio Dev C
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
