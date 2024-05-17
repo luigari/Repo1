4fb4d9f3a9e83eda54eb7f227abb5f0e9e21492a
luigari <agalvisr@gmail.com>
2024-05-17 14:03:20 -0500

    Development (#5)
    * Cambio A sobre Dev
    
    * Update CartItem.cs
    
    Cambio 1
    
    * Update Category.cs

--- src/PartsUnlimited.Models/Category.cs
+++ src/PartsUnlimited.Models/Category.cs
@@ -1,6 +1,6 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
-
+//Cambio 2
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 //Otro cambio
